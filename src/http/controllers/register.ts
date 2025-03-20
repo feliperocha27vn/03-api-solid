f2c209180788df0baed918920c492548b31c4211
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-03-20 14:07:11 -0300

    first commit

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,30 @@
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+  try {
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
