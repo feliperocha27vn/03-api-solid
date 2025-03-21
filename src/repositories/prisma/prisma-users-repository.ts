fd0dda729c87cddfcd7b472d6679b82ad268b871
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-03-21 12:00:03 -0300

    interface do repositório

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import type { Prisma } from '@prisma/client'
 import type { UsersRepository } from '../user-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
