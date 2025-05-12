4c91ffbb45eb2700396a987ea8ac73d826897c38
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-05-12 11:38:11 -0300

    feat: caso de uso autenticação

---
+++ src/use-cases/athenticate.ts
@@ -0,0 +1,35 @@
+import type { UsersRepository } from '@/repositories/user-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import type { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ email, password }: AuthenticateUseCaseRequest) {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
