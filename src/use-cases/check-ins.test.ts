eb28c63057efb42ac6372427956cde828f173671
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-05-22 17:32:50 -0300

    tdd e mocking

--- src/use-cases/check-ins.test.ts
+++ src/use-cases/check-ins.test.ts
@@ -1,4 +1,4 @@
-import { expect, it, describe, beforeEach } from 'vitest'
+import { expect, it, describe, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './checkin'
 import { InMemoryCheckInsRepository } from '@/in-memory/in-memory-checkin-repository'
 
@@ -9,6 +9,12 @@ describe('Register use case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
@@ -17,6 +23,22 @@ describe('Register use case', () => {
       userId: '01',
     })
 
-    await expect(checkIn.id).toEqual(expect.any(String))
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2020, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: '01',
+      userId: '01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: '01',
+        userId: '01',
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
