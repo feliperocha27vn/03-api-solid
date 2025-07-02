d609b1f97b9ec5e6b8af1e6e41723dc193deb0a2
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-07-02 12:14:02 -0300

    feat: Controller de criação de academia

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,34 @@
+import { makeCreateGymUseCase } from '@/facotories/make-create-gym-use-case'
+import { makeRegisterUseCase } from '@/facotories/make-register-use-case'
+import { UserAlreadyExists } from '@/use-cases/errors/user-already-exists-error'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { title, description, phone, latitude, longitude } =
+    createGymBodySchema.parse(request.body)
+
+  const createGymUseCase = makeCreateGymUseCase()
+
+  await createGymUseCase.execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  })
+
+  return reply.status(201).send()
+}
